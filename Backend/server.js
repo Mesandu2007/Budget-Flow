a329b91f70f88930cb30c2480e2784eb8ebf3194
Mesandu2007 <mesandugunarwardhana@gmail.com>
2026-07-18 17:16:51 +0530

    added gitignore

--- Backend/server.js
+++ Backend/server.js
@@ -8,13 +8,18 @@ dotenv.config();
 
 const app = express();
 
-
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      process.env.FRONTEND_URL
+    ],
     credentials: true,
   })
 );
+
+
+
 
 app.use(express.json());
 app.use(passport.initialize());
